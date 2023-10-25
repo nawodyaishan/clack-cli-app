cfe3cce91bc8e5d7c2b51a9ee7c1de15c6ad2533
Nawodya Ishan <nawodyain@gmail.com>
2023-10-25 23:01:02 +0530

    feat: question data

---
+++ src/data/questions-data.ts
@@ -0,0 +1,62 @@
+export const questionsData: Question[] = [
+    new Question({
+        question: "What year was the game 'The Legend of Zelda' first released?",
+        answersArray: ["1985", "1990", "1995", "2000"],
+        correctAnswerIndex: 0,
+    }),
+
+    new Question({
+        question: "In which game do players build and explore their own virtual worlds?",
+        answersArray: ["Minecraft", "Fortnite", "Roblox", "Terraria"],
+        correctAnswerIndex: 2,
+    }),
+
+    new Question({
+        question: "Which game features a battle royale mode called 'Warzone'?",
+        answersArray: ["Fortnite", "PUBG", "Call of Duty: Modern Warfare", "Apex Legends"],
+        correctAnswerIndex: 2,
+    }),
+
+    new Question({
+        question: "What is the main character's name in the 'Half-Life' series?",
+        answersArray: ["Gordon Freeman", "John Doe", "Alex Mercer", "Master Chief"],
+        correctAnswerIndex: 0,
+    }),
+
+    new Question({
+        question: "Which game features the phrase 'Finish Him!' during fights?",
+        answersArray: ["Street Fighter", "Mortal Kombat", "Tekken", "Super Smash Bros."],
+        correctAnswerIndex: 1,
+    }),
+
+    new Question({
+        question: "What is the primary objective in the game 'Among Us'?",
+        answersArray: ["Complete tasks", "Survive", "Kill other players", "Build structures"],
+        correctAnswerIndex: 0,
+    }),
+
+    new Question({
+        question: "Which game is known for its blocky, pixelated graphics and crafting mechanics?",
+        answersArray: ["Stardew Valley", "Terraria", "Minecraft", "Don't Starve"],
+        correctAnswerIndex: 2,
+    }),
+
+    new Question({
+        question: "In 'The Elder Scrolls V: Skyrim,' what is the famous line shouted by dragons?",
+        answersArray: ["Fus Ro Dah", "Hakuna Matata", "Avada Kedavra", "Kamehameha"],
+        correctAnswerIndex: 0,
+    }),
+
+    new Question({
+        question: "What is the main objective of 'The Witcher 3: Wild Hunt'?",
+        answersArray: ["Solve mysteries", "Hunt monsters", "Find treasure", "Rescue princesses"],
+        correctAnswerIndex: 1,
+    }),
+
+    new Question({
+        question: "Which game is set in the post-apocalyptic wasteland of the Commonwealth?",
+        answersArray: ["Fallout 4", "Bioshock Infinite", "The Last of Us", "Metro Exodus"],
+        correctAnswerIndex: 0,
+    }),
+];
+
