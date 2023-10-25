b86c5729f4a25cac4a3c00233b275cefc451b655
Nawodya Ishan <nawodyain@gmail.com>
2023-10-25 23:38:35 +0530

    feat: QuestionHelper

---
+++ src/helpers/question-helper.ts
@@ -0,0 +1,27 @@
+import * as p from '@clack/prompts'
+import {IQuestion} from "../features/question";
+import {StateStore} from "../features/StateScore";
+
+export abstract class QuestionHelper {
+    static async askQuestion(question: IQuestion) {
+        const options: { value: string; label?: string | undefined; hint?: string | undefined; }[] = []
+        const stateStore = StateStore.getInstance();
+
+        question.answersArray.map(answer => {
+            options.push({
+                value: answer,
+                label: answer
+            })
+        })
+
+        const answer = await p.select({
+            message: question.question,
+            options: options,
+            initialValue: '1'
+        })
+
+        if (answer === question.answersArray[question.correctAnswerIndex]) {
+            stateStore.increment()
+        }
+    }
+}
