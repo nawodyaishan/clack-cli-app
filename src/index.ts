199044def2773401945f7244f5205d3e3a0fa6f2
Nawodya Ishan <nawodyain@gmail.com>
2023-10-25 23:42:09 +0530

    feat: refactor

--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,101 @@
-import * as p from '@clack/prompts'
-import color from 'picocolors'
+import * as p from '@clack/prompts';
+import color from 'picocolors';
 import {QuestionHelper} from "./helpers/question-helper";
 import {questionsData} from "./data/questions-data";
 import {StateStore} from "./features/StateScore";
 
-const main = async () => {
-    console.clear();
-    p.intro(`${color.bgMagenta(color.black('Trivia Quiz'))}`)
+const TOTAL_PROGRESS = 10000;
+const QUIZ_TITLE = 'Trivia Quiz';
+const LOADING_INTERVAL = 100;
+const REPLAY_DELAY = 10000;
 
+const displayIntro = (title: string) => {
+    p.intro(`${color.bgMagenta(color.black(title))}`);
+};
+
+const displayOutro = (message: string) => {
+    p.outro(`${color.bgMagenta(color.black(message))}`);
+};
+
+const loadPackages = async (): Promise<void> => {
     const spin = p.spinner();
-    const total = 10000;
     let progress = 0;
     spin.start();
 
-    new Promise((resolve) => {
+    await new Promise<void>((resolve) => {
         const timer = setInterval(() => {
-            progress = Math.min(total, progress + 1);
-            if (progress >= total) {
+            progress = Math.min(TOTAL_PROGRESS, progress + 800);
+            spin.message(`Loading packages [${progress}/${TOTAL_PROGRESS}]`);
+            if (progress >= TOTAL_PROGRESS) {
                 clearInterval(timer);
-                resolve(true);
+                resolve();
             }
-            spin.message(`Loading packages [${progress}/${total}]`); // <===
-        }, 100);
-    }).then(() => {
-        spin.stop(`Done`);
-        p.outro('spinner stop...');
+        }, LOADING_INTERVAL);
     });
 
-    setTimeout(() => {
-        p.intro(`${color.bgMagenta(color.black('Trivia Quiz'))}`)
-    }, 10000);
-    const stateStore = StateStore.getInstance();
-    const readyToPlay = await p.select({
-        message: "No cheating. 10 questions. Results at the end. Ready to play?",
-        initialValue: "Yes",
-        options: [
-            {value: "Yes", label: "Yes"},
-            {value: "No", label: "No"}],
-    })
+    spin.stop('Done');
+    displayOutro('spinner stop...');
+};
 
-    if (readyToPlay == "Yes") {
-        // Begin trivia game
-        questionsData.map(question => {
-            QuestionHelper.askQuestion(question).then(() => console.clear())
-        })
-
-        // Decide what ending screen to show based on how many questions user answered correctly
-        p.outro(`${color.bgMagenta(color.black(`You got ${stateStore.getState()} questions correct!`))}`);
-
-        if (stateStore.getState() == 10) {
-            const s = p.spinner();
-            setTimeout(() => {
-                s.start("Generating secret message");
-            }, 5000);
-            s.stop();
-            p.outro(`${color.bgMagenta(color.black(`The command line is a tool that is ripe for change. `))}`);
-        } else {
-            const s = p.spinner();
-            setTimeout(() => {
-                s.start();
-            }, 3000);
-            s.stop();
-            p.outro(`${color.bgMagenta(color.black(`You need 10/10 correct to unlock the secret message. Try again.`))}`);
-        }
-    } else {
-        p.outro(`${color.bgMagenta(color.black(`Ok. Bye!`))}`);
+const askQuestions = async (): Promise<void> => {
+    for (const question of questionsData) {
+        await QuestionHelper.askQuestion(question);
+        console.clear();
     }
-}
+};
 
-main().catch(console.error);
+const displayResult = (): void => {
+    const stateStore = StateStore.getInstance();
+    const score = stateStore.getState();
+    displayOutro(`You got ${score} questions correct!`);
+
+    const s = p.spinner();
+
+    if (score === 10) {
+        setTimeout(() => {
+            s.start("Generating secret message");
+            s.stop();
+            displayOutro(`The command line is a tool that is ripe for change.`);
+        }, 5000);
+    } else {
+        setTimeout(() => {
+            s.start();
+            s.stop();
+            displayOutro(`You need 10/10 correct to unlock the secret message. Try again.`);
+        }, 3000);
+    }
+};
+
+const main = async () => {
+    try {
+        console.clear();
+        displayIntro(QUIZ_TITLE);
+
+        await loadPackages();
+
+        setTimeout(() => {
+            displayIntro(QUIZ_TITLE);
+        }, REPLAY_DELAY);
+
+        const readyToPlay = await p.select({
+            message: "No cheating. 10 questions. Results at the end. Ready to play?",
+            initialValue: "Yes",
+            options: [
+                {value: "Yes", label: "Yes"},
+                {value: "No", label: "No"}
+            ],
+        });
+
+        if (readyToPlay == "Yes") {
+            await askQuestions();
+            displayResult();
+        } else {
+            displayOutro(`Ok. Bye!`);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+main();
+
